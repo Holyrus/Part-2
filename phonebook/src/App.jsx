1df9cc657ade6cd9917fdfe67419f50078bb3894
Veronika Pozdniakova <800053068@omnia.fi>
2024-11-27 02:45:03 +0200

    Completed 2.11

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,17 +1,24 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
+import axios from 'axios';
 
 const App = () => {
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '12312415151', id: 1 },
-    { name: 'Ada Lovelace', number: '123124122351', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
+
+  useEffect(() => {
+    console.log('Effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('Promise fulfilled')
+        setPersons(response.data);
+      })
+  }, [])
+  console.log('rendered', persons.length, 'persons')
 
   const handleAddNumber = (event) => {
     event.preventDefault();
