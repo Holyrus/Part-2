6292e5e7533df533ce4cff03cf95962047abd9a3
Veronika Pozdniakova <800053068@omnia.fi>
2024-11-30 04:30:14 +0200

    Creating countries app

---
+++ countries/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react'
+
+const App = () => {
+
+
+  return (
+    <div>
+      <label htmlFor="search">Find countries </label>
+      <input id="search" />
+
+    </div>
+  )
+}
+
+export default App
